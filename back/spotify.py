fc0a52f5397044f1e5bbcf898c20f08652bc57ce
Sergey Kopylov <sergkop@gmail.com>
2025-01-26 19:14:48 +0100

    Spotify credentials fallback

--- back/spotify.py
+++ back/spotify.py
@@ -4,8 +4,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 
 class SpotifySettings(BaseSettings):
-    client_id: str = ''
-    secret_id: str = ''
+    client_id: str = 'placeholder'
+    secret_id: str = 'placeholder'
 
     class Config:
         env_prefix = 'SPOTIFY_'
@@ -15,6 +15,6 @@ spotify_settings = SpotifySettings()
 
 
 spotify_api = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=spotify_settings.client_id or 'placeholder',
-    client_secret=spotify_settings.secret_id or 'placeholder',
+    client_id=spotify_settings.client_id,
+    client_secret=spotify_settings.secret_id,
 ))
