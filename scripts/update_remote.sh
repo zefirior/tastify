d424953490b096b42b01bf6f815a5faf22ead161
Daniil Galiev <wizard.liinda@gmail.com>
2025-01-26 02:20:11 +0100

    migrate remote

--- scripts/update_remote.sh
+++ scripts/update_remote.sh
@@ -5,7 +5,7 @@ set -e
 cd tastify
 
 git fetch
-git reset --hard origin/remote
+git reset --hard origin/main
 
 docker compose --file ./docker/remote.docker-compose.yaml stop
 docker compose --file ./docker/remote.docker-compose.yaml up --build -d
