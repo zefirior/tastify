fb31f847596158e8d06accad3b9d3adecfd874f1
Daniil Galiev <wizard.liinda@gmail.com>
2025-03-09 18:28:48 +0100

    fix layout for mobile (#26)

--- front/src/components/room/DashNew.jsx
+++ front/src/components/room/DashNew.jsx
@@ -3,14 +3,28 @@ import * as React from 'react';
 import PlayersGrid from '../PlayersGrid.jsx';
 import QRCode from 'react-qr-code';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import {Alert, AlertTitle, Backdrop, Paper} from '@mui/material';
+import Typography from '@mui/material/Typography';
 
 export default function DashNew({room}) {
     const [starting, setStarting] = React.useState(false);
+    const [showAlert, setShowAlert] = React.useState(false);
+
     async function start() {
+        if (room.players.length < 2) {
+            setShowAlert(true);
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 2000);
+            return;
+        }
+
         setStarting(true);
         setTimeout(() => {
             setStarting(false);
-        }, 2000);
+        }, 5000);
         await Client.startGame(room.code).then(() => {
             console.log('game started');
         });
@@ -18,24 +32,46 @@ export default function DashNew({room}) {
 
 
     return (
-        <>
-            <h1>Game dashboard: {room.code}</h1>
-            <h2>Scan to join</h2>
-            <div className="qr-back pt-20 background-white p-4 m-4 rounded-lg">
-                <QRCode
-                    title="Scan to join"
-                    value={`${import.meta.env['VITE_BASE_URL']}/room/${room.code}/join`}
-                />
-            </div>
-            <Button
-                variant="outlined"
-                color="primary"
-                size="small"
-                loading={starting}
-                sx={{flexShrink: 0}}
-                onClick={start}
-            >Start game</Button>
-            <PlayersGrid room={room} />
-        </>
+        <Stack spacing={2}>
+            <Backdrop
+                sx={(theme) => ({ zIndex: theme.zIndex.drawer + 1 })}
+                open={showAlert}
+                onClick={() => setShowAlert(false)}
+            >
+                <Paper>
+                    <Alert variant="filled" severity="info" sx={{minHeight: '100px'}}>
+                        Need at least 2 players to start the game.
+                    </Alert>
+                </Paper>
+            </Backdrop>
+            <Paper variant="elevation" color={'blue'} className="p-4 m-20">
+                <Stack>
+                    <Box>
+                        <Typography variant="h5" component="div">
+                            Scan to join room: {room.code}
+                        </Typography>
+                        <div className="qr-back pt-20 background-white p-4 m-4 rounded-lg">
+                            <QRCode
+                                title="Scan to join"
+                                value={`${import.meta.env['VITE_BASE_URL']}/room/${room.code}/join`}
+                            />
+                        </div>
+                    </Box>
+                    <Box>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            size="large"
+                            loading={starting}
+                            sx={{flexShrink: 0, margin: '10px'}}
+                            onClick={start}
+                        >Start game</Button>
+                    </Box>
+                </Stack>
+            </Paper>
+            <Paper color="blue" className="p-4 m-20">
+                <PlayersGrid room={room} />
+            </Paper>
+        </Stack>
     );
 }
