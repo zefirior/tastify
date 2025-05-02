ab6abad8d20219e547f5f5678eb5bb685035b070
perminovsi <nplusone.dx@gmail.com>
2025-05-02 15:31:33 +0200

    minor

--- front/src/components/room/submit-track/SuggesterSubmitTrack.jsx
+++ front/src/components/room/submit-track/SuggesterSubmitTrack.jsx
@@ -1,12 +1,20 @@
-import * as React from 'react';
-
 export default function SuggesterSubmitTrack({room}) {
     const groupName = room.state.currentRound.groupName;
+    
+    const sarcasticPhrases = [
+        "Ah yes, your musical genius strikes again! 🎵",
+        "Look at you, expanding everyone's musical horizons! ✨",
+        "Another masterpiece added to the playlist! 🎧",
+        "Your music taste is... let's say, adventurous! 🌟",
+        "Well, this will definitely get the party started! 🎉",
+    ];
+
+    const randomPhrase = sarcasticPhrases[Math.floor(Math.random() * sarcasticPhrases.length)];
 
     return (
         <>
             <div>You suggest {groupName}</div>
-            <div>Please wait your friends</div>
+            <div>{randomPhrase}</div>
         </>
     );
 }
